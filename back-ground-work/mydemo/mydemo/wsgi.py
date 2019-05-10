85679f8cde1b1b26c76c8366715c5cba0d090f72
walikrence <969327526@qq.com>
2019-05-11 00:20:36 +0800

    auto

--- back-ground-work/mydemo/mydemo/wsgi.py
+++ back-ground-work/mydemo/mydemo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myweb project.
+WSGI config for mydemo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydemo.settings')
 
 application = get_wsgi_application()
